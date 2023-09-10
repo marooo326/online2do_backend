6477190d7151d343db0e7f5fa1a2fa9522588d8b
marooo326 <ktnqkr01@gmail.com>
2023-09-10 18:25:51 +0900

    fix: AuthController Login Mapping 수정

--- src/main/java/com/marooo/todo/auth/controller/AuthController.java
+++ src/main/java/com/marooo/todo/auth/controller/AuthController.java
@@ -4,7 +4,7 @@ import com.marooo.todo.auth.dto.Jwt;
 import com.marooo.todo.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final AuthService authService;
 
-    @RequestMapping("/login")
+    @PostMapping("/login")
     public ResponseEntity<Jwt> login(@RequestParam String username) {
         return ResponseEntity.ok(authService.login(username));
     }
